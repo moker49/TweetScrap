2f34fbc06a4be247252b7bd6878297e53a84f5a0
Efren <moker49@gmail.com>
2023-07-13 20:15:58 -0400

    new

--- scrap.py
+++ scrap.py
@@ -1,27 +1,22 @@
+from lxml import html
 import requests
-import pandas as pd
 
-twitter_data = []
 
-payload = {
-    'api_key': 'YOUR_API_KEY',
-    'query': 'sentiment analysis',
-    'num': '3',
-    'time_period': '1D'
-}
-response = requests.get(
-    'https://api.scraperapi.com/structured/twitter/search', params=payload)
-data = response.json()
+page = requests.get(
+    'https://mentalmars.com/game-news/borderlands-3-golden-keys/')
+tree = html.fromstring(page.content)
 
-all_tweets = data['tweets']
-for tweet in all_tweets:
-    twitter_data.append({
-        'ID': tweet['tweet_id'],
-        'User': tweet["user"],
-        'Tweet': tweet["text"],
-        'URL': tweet["link"]
-    })
+test = tree.xpath(
+    '//*[@id="gp-content"]/article/div[5]/figure[1]/table/tbody/tr[1]/td[3]')
 
-df = pd.DataFrame(twitter_data)
-df.to_json('scraped_tweets.json', orient='index')
-print('Tweets exported to JSON')
+reward = tree.xpath(
+    '//*[@id="gp-content"]/article/div[5]/figure[1]/table/tbody/tr[1]/td[1]/strong/text()')
+expire_date = tree.xpath(
+    '//*[@id="gp-content"]/article/div[5]/figure[1]/table/tbody/tr[1]/td[2]/text()')
+last_key = tree.xpath(
+    '//*[@id="gp-content"]/article/div[5]/figure[1]/table/tbody/tr[1]/td[3]/code/text()')
+
+
+print(reward)
+print(expire_date)
+print(last_key)
