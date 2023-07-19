ca2621235066856503b65f8e2fbc3c9836ec69d9
Efren <moker49@gmail.com>
2023-07-19 18:30:13 -0400

    channel list

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 import { Client, IntentsBitField, EmbedBuilder } from 'discord.js';
 
-let shift_channel;
-const SHIFT_CHANNEL_ID = '321789236795670528'
 const COLOR_SHIFT = 0xf4c310;
 
 const client = new Client({
@@ -16,7 +14,8 @@ client.login(process.env.DISCORD_TOKEN);
 
 client.on("ready", () => {
     console.log(`${client.user.tag} online.`)
-    shift_channel = client.channels.cache.find(c => c.id === SHIFT_CHANNEL_ID);
+    guild = client.guilds.cache.find
+    
     setInterval(checkKey, 600000);
 });
 
@@ -25,9 +24,15 @@ function checkKey(){
 	let last = JSON.parse(data);
     if (last.status === 'pending'){
         console.log('syncing new tweet')
+        loadChannels()
         sendMessage(last)
         updateKey(last)
     }
+}
+
+function loadChannels(){
+    let data = fs.readFileSync("channels.json", "utf-8");
+	channels = JSON.parse(data);
 }
 
 function sendMessage(last){
@@ -38,8 +43,15 @@ function sendMessage(last){
 		.setTitle(':GoldenKey: Borderlands 2 | Borderlands 3 | Wonderlands')
 		.setDescription(keyMsg)
         .setFooter({ text: `${last.expire_date}`, iconURL: 'https://i.imgur.com/GBh8nCB.png' });
-    shift_channel.send('@SHiFT Codes');
-    shift_channel.send({embeds: [embed_msg]});
+
+    let data = fs.readFileSync("channels.json", "utf-8");
+    let channels = JSON.parse(data);
+
+    channels.forEach(function(shift_channel) {
+        shift_channel = client.channels.cache.find(c => c.id === SHIFT_CHANNEL_ID);
+        shift_channel.send('@SHiFT Codes');
+        shift_channel.send({embeds: [embed_msg]});
+    });
 }
 
 function updateKey(last){
